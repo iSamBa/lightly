abc845a847df5b1eddacc74614de60f90a440128
iSamBa <bahou.aissam@gmail.com>
2021-02-22 21:06:29 +0100

    create homepage

---
+++ client/src/components/Header.js
@@ -0,0 +1,30 @@
+import "./header.css";
+import { Button } from "semantic-ui-react";
+
+function Header() {
+  return (
+    <div>
+      <div className="header-container">
+        <div className="nav">
+          <div className="brand">lightly</div>
+          <div className="items">
+            <Button>Login</Button>
+            <Button>Register</Button>
+          </div>
+        </div>
+        <div className="introduction">
+          <h1 className="title">Lightly</h1>
+          <h4 className="description">
+            This tool will allow you to add and manage light controllers of your
+            home
+          </h4>
+          <Button primary className="start-btn">
+            Get started !
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
