d1a6f9adf7e562641c52e4fd365854f07742e0db
iSamBa <bahou.aissam@gmail.com>
2021-02-23 15:48:03 +0100

    refactor routes

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const ApiRouter = require("./routes/api");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -9,8 +10,5 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.status(200).send("Welcome to home page");
-});
-
+app.use("/api", ApiRouter);
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT} ...`));
