f1065b230209346c0559c837daf60d5485c72150
iSamBa <bahou.aissam@gmail.com>
2021-02-23 14:18:45 +0100

    set basic server up

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const PORT = 5000 | process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to home page");
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT} ...`));
