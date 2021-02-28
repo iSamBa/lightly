fbb0114c41cda9c5049c365601f4222bc21a3ad5
iSamBa <bahou.aissam@gmail.com>
2021-02-28 20:02:09 +0100

    change the paragraph font

--- client/src/components/GettingStarted.js
+++ client/src/components/GettingStarted.js
@@ -8,10 +8,8 @@ const GettingStarted = () => {
       <div className="introduction">
         <h1 className="title">Lightly</h1>
         <h4 className="description">
-          With <span className="brand">Lightly</span> you will have a single
-          point of control of all the light bridges in your house, you can
-          manage and parameterize all of them from{" "}
-          <span className="brand">Lightly</span>
+          Manage all the light controlling systems of your house from a single
+          place
         </h4>
         <Button color="black" className="start-btn">
           Get started !
