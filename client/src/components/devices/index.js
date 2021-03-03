c02976d69f3f44fb83f4e1c7066f191822869b9c
iSamBa <bahou.aissam@gmail.com>
2021-03-03 16:13:47 +0100

    add button to add new device

--- client/src/components/devices/index.js
+++ client/src/components/devices/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Card, Header, Icon } from "semantic-ui-react";
+import { Card, Header, Icon, Button } from "semantic-ui-react";
 import Device from "./device";
 import "./devices.css";
 
@@ -12,14 +12,16 @@ const Devices = () => {
   }, []);
   return (
     <div className="devices-container">
-      <Header className="devices-header" as="h2">
+      <Header className="devices-header">
         <Icon name="settings" />
         <Header.Content>
-          Devices
+          <div>
+            <h2>Devices</h2>
+            <Button>Add new device</Button>
+          </div>
           <Header.Subheader>Manage your preferences</Header.Subheader>
         </Header.Content>
       </Header>
-      <hr></hr>
       <Card.Group className="devices-group">
         {devices.map((device) => (
           <Device key={device._id} device={device} />
