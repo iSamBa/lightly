2eb88e3a4be3d0fe580af9b7179d612430996170
iSamBa <bahou.aissam@gmail.com>
2021-03-04 20:29:45 +0100

    implement POST device method

--- client/src/components/devices/add-device.js
+++ client/src/components/devices/add-device.js
@@ -1,23 +1,51 @@
-import React from "react";
-import { Button, Checkbox, Form } from "semantic-ui-react";
+import { useState } from "react";
+import { Button, Form } from "semantic-ui-react";
 
 const AddDevice = () => {
+  const [name, setName] = useState("");
+  const [type, setType] = useState("");
+  const [description, setDescription] = useState("");
+  const handleClick = () => {
+    fetch("http://localhost:5000/api/devices", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, type, description }),
+    });
+  };
   return (
     <Form>
       <Form.Field>
         <label>Name</label>
-        <input placeholder="Device Name" />
+        <input
+          placeholder="Device Name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
       </Form.Field>
       <Form.Field>
         <label>Type</label>
-        <input placeholder="Device Type" />
+        <input
+          placeholder="Device Type"
+          onChange={(e) => {
+            setType(e.target.value);
+          }}
+        />
       </Form.Field>
       <Form.Field>
         <label>Description</label>
-        <input placeholder="Device Description" />
+        <input
+          placeholder="Device Description"
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+        />
       </Form.Field>
-      <Button primary type="submit">
-        Submit
+      <Button primary onClick={handleClick}>
+        Add
       </Button>
     </Form>
   );
